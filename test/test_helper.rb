d46b1bc766394184dd22c633c64913daa25ec6af
jiikko <n905i.1214@gmail.com>
2019-01-30 19:30:20 +0900

    テスト時にredisを起動する

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,12 +8,45 @@ class Minitest::TestWithRedis < Minitest::Test
   include Minitest::Hooks
 
   def before_all
-    puts 'start redis'
-    # TODO
+    RedisManeger.start
   end
 
   def after_all
-    puts 'shutdown redis'
-    # TODO
+    RedisManeger.shutdown
+  end
+end
+
+
+class RedisManeger
+  require 'fileutils'
+  root = File.expand_path("../..", __FILE__)
+  REDIS_PID = "#{root}/tmp/pids/redis-test.pid".freeze
+  REDIS_CACHE_PATH = "#{root}/tmp/cache/".freeze
+
+  def self.start
+    FileUtils.mkdir_p(['./tmp/pids', './tmp/cache'])
+
+    redis_options = {
+      'daemonize'     => 'yes',
+      'pidfile'       => REDIS_PID,
+      'port'          => 9_736,
+      'timeout'       => 300,
+      'save 900'      => 1,
+      'save 300'      => 1,
+      'save 60'       => 10_000,
+      'dbfilename'    => 'dump.rdb',
+      'dir'           => REDIS_CACHE_PATH,
+      'loglevel'      => 'debug',
+      'logfile'       => 'stdout',
+      'databases'     => 16
+    }.map { |k, v| "#{k} \"#{v}\"" }.join("\n")
+    `echo '#{redis_options}' | redis-server -`
+  end
+
+  def self.shutdown
+    `
+      cat "#{REDIS_PID}" | xargs kill -QUIT
+      rm -f "#{REDIS_CACHE_PATH}dump.rdb"
+    `
   end
 end
